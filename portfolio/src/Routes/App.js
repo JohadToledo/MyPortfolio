ffe983e6079eadb06fa432f3edde073c47fc64b6
Johad Toledo <johadtoledo@gmail.com>
2022-03-16 20:56:20 +0100

    add Material ui

--- portfolio/src/Routes/App.js
+++ portfolio/src/Routes/App.js
@@ -1,5 +1,5 @@
 
-import "../App.css";
+import "../assets/css/style.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../container/Home";
@@ -8,6 +8,17 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+
+      <script
+        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+        crossorigin></script>
+      
+      <script
+        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin></script>
+      
+      <script>var Alert = ReactBootstrap.Alert;</script>
         <Layout>
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer class="mediumspace center">
+      <footer className="mediumspace center">
         All rights reserved. Design, content and development by Johad Toledo.
         2021.
       </footer>
