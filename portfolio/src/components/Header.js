13701cd7f412e88afa6412f7f0e0fdb9c8d88e1b
Johad Toledo <johadtledo@gmail.com>
2022-03-17 13:48:21 +0100

    add some style on header file

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
+import logo from "../assets/statics/JT-logo.png";
+import linkedin from "../assets/statics/linkedin.svg";
+import github from "../assets/statics/github.svg";
+import "../assets/css/Header.css";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function Hearder() {
+export default function Header() {
   return (
     <>
-        <div className="header">
-          <div>Logo</div>
-        
+      <div className="header">
+        <div>
+          <Link to="/">
+            <img className="jtlogo" src={logo} alt="" />
+          </Link>
         </div>
+        <div>
+          <a href="https://www.linkedin.com/in/johad-toledo-webdeveloper/">
+            <img className="logoHeader" src={linkedin} alt="" />
+          </a>
+          <a href="https://github.com/JohadToledo">
+            <img className="logoHeader" src={github} alt="" />
+          </a>
+        </div>
+      </div>
     </>
   );
 }
