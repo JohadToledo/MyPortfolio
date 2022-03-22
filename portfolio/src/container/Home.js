562f8b42aaff9a3b9c2e0bfcf0a04b901f671d04
Johad Toledo <johadtledo@gmail.com>
2022-03-22 10:10:08 +0100

    Add some style to hero section

--- portfolio/src/container/Home.js
+++ portfolio/src/container/Home.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from "react";
 import "../assets/css/style.css";
-
 
 export default function Home() {
   return (
-    <div className="container">
+    <section className='content hero space100'>
+      <div className="container">
+        <div className="row space50">
+          <div className="col-md-8">
+            <h1>Hey! I am Johad</h1>
+            <h2>
+              A <b className="underline">Web developer</b> building
+            </h2>
+          </div>
+          <div className="col-md-8">
+              <h2><b className="underline">creative</b> things.</h2>
+          </div>
+        </div>
         <div className="row">
-            <div className="col-md-8">
-                <h1>Hey! I am Johad</h1>
-                <h2>
-                    A <b className="underline">Web developer</b> building
-                    <b className="underline">creative</b> things.
-                </h2>
-            </div>
+          <div className="col-md-6 offset-md-3">
+            A Junior Web Developer, who is starting his journey so eagerly
+          </div>
+          <div className="button center mediumspace">
+            <button type="" className="btn btn-lg">
+              <a href="contact.html">
+                <strong>Contact</strong>
+              </a>
+            </button>
+          </div>
         </div>
-        <div className="row space ">
-            <div className="col-md-6 offset-md-3">A Junior Web Developer, who is starting his journey so eagerly</div>
-            <div className="button center mediumspace">
-                <button type="" className="btn btn-lg"><a href="contact.html"><strong>Contact</strong></a></button>       
-            </div> 
-        </div>
-  </div>
-  )
+      </div>
+    </section>
+  );
 }
